a47200ebb47b78fc8a3cdb18930a9a97d3f9c6e1
Manikandan S <ceitmanikandan24@gmail.com>
2026-02-03 15:29:47 +0530

    Add DELETE request

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,9 +1,11 @@
 import exp from "express";
 import user from "./user.json" assert { type: "json" };
+import cors from "cors";
 
 const app = exp();
 const port=3000;
-app.use(exp.json())
+//app.use(exp.json());
+app.use(cors())
 
 app.listen(port,(err)=>{
     if(err){
@@ -14,10 +16,11 @@ app.listen(port,(err)=>{
 app.get('/',(req,res)=>{
     res.send({message:"root"})
 });
-
+// send all user
 app.get('/api/user',(req,res)=>{
     res.send(user);
 });
+//send user by id
 app.get('/api/user/:id',(req,res)=>{
     const id=parseInt(req.params.id);
     console.log(id);
@@ -29,4 +32,22 @@ app.get('/api/user/:id',(req,res)=>{
         return res.send(getUser);
     }
     return res.status(402).send({message:"user not found"});
+})
+
+//delete user
+app.delete('/api/user/:id',(req,res)=>{
+    const id=parseInt(req.params.id)
+    console.log(id);
+    if (isNaN(id)){
+        return res.status(400).send({message:"invalide request or user"})
+    }
+    const updateUser=user.filter((user)=>user.id !==id)
+      const exists = user.some(u => u.id === id);
+
+  if (!exists) {
+    return res.status(404).json({ message: "User not found" });
+  }
+    res.send(updateUser)  
+    
+    
 })
